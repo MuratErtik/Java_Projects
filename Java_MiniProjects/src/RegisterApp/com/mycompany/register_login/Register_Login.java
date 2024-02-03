b0d7f8e01321739c371d16bfffe5a86f50578f0e
Murat Ertik <dmuratertik1@gmail.com>
2024-02-03 15:34:11 +0300

    RegisterApp
    I developed a mini project with Swing

---
+++ Java_MiniProjects/src/RegisterApp/com/mycompany/register_login/Register_Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.register_login;
+
+/**
+ *
+ * @author dmura
+ */
+public class Register_Login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
